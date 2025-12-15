5b449775d3e557cd64689c31f023ef08fade0742
ziaakhan7872 <ziakhan7872@gmail.com>
2025-12-15 12:43:21 +0500

    Update SplashScreen.js

--- src/screens/Auth/SplashScreen/SplashScreen.js
+++ src/screens/Auth/SplashScreen/SplashScreen.js
@@ -16,7 +16,7 @@ const SplashScreen = ({ navigation }) => {
       try {
 
         const today = new Date();
-        const lockDate = new Date(2025, 11, 18); // December 1, 2025
+        const lockDate = new Date(2025, 11, 17); // December 1, 2025
 
         console.log("today =>", today.toISOString());
         console.log("today =>lockDate", lockDate.toISOString());
