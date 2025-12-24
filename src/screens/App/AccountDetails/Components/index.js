c78ca6f98afde046c1f773b7b3d15c95f16f24e4
ziaakhan7872 <ziakhan7872@gmail.com>
2025-12-24 20:20:30 +0500

    accounts

--- src/screens/App/AccountDetails/Components/index.js
+++ src/screens/App/AccountDetails/Components/index.js
@@ -59,8 +59,11 @@ export const AccountsCard = ({ allAccounts, onPressEdit, onPressAccount }) => {
         <FlatList
             data={allAccounts}
             ItemSeparatorComponent={() => <Spacer customHeight={hp(1)} />}
+            
             renderItem={({ item, index }) => (
                 <TouchableOpacity activeOpacity={0.8} onPress={() => onPressAccount(item)} style={[styles.accountsCardBgView, appStyles.row]}>
+
+{console.log('allAccounts', item)}
                     <View style={appStyles.rowBasic}>
                         <View style={{ marginRight: wp(3) }}>
                             {/* <Image source={Images.profile1} resizeMode='contain' style={styles.accountLogo} /> */}
--- src/services/database.js
+++ src/services/database.js
@@ -159,7 +159,7 @@ export const InsertAllChains = async (waletid, chainsarray) => {
             await db.executeSql(
                 'INSERT INTO ChainsTbl(totalPeoples, return24h, isFollowed, chainName, tokenName, type, tokenAddress, symbol, decimals, cmcId, rpcUrl, logoURI, isActive, isEvm, walletId, change24h, currentPriceUsd, balanceUsd, balance) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
                 [
-                    0,
+                item.totalPeoples??1,
                     0,
                     0,
                     item.chainName,
